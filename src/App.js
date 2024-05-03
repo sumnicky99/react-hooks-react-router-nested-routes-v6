41c81d06925e5ecb7416d405363d00d00a650bd2
Sumnicky99 <sumnicky99@gmail.com>
2024-05-03 12:59:33 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
+import { Outlet } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import { useState, useEffect } from "react";
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() =>{
+      fetch("http://localhost:4000/users")
+      .then(r => r.json())
+      .then(data => setUsers(data))
+      .catch(error => console.error(error));
+  }, []);
   return (
     <>
       <header>
-        
+        <NavBar />
       </header>
+      <Outlet context={users}/>
     </>
   );
 };
--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -4,7 +4,9 @@ function UserCard({user}) {
     return (
       <article>
           <h2>{user.name}</h2>
+          <p>
           <Link to={`/profile/${user.id}`}>View profile</Link>
+          </p>
       </article>
     );
   };
